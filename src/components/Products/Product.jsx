0e826f13a00753cd55dfad2a7f4097043ae8279d
tajikwizard <abdulazizcodeshere@gmail.com>
2024-04-22 16:26:37 +0500

    hover effect om card

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -4,24 +4,27 @@ import { Link } from "react-router-dom";
 
 const Product = ({ id, title, price, rating, image }) => {
   return (
-    <Link to={`/product/${id}`}>
-      <div className=" hover:cursor-pointer w-[300px] hover:shadow-sm wrapper flex flex-col justify-center items-center max-w-xs rounded overflow-hidden shadow-lg sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
-        <img className="w-32 h-42" src={image} alt="Product" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-center">{title}</div>
-        </div>
-        <div className="container flex justify-around">
-          <span>⭐ Rating:{rating.rate}</span>
-          <span>Count:{rating.count}</span>
-        </div>
-        <div className="px-6 py-4 w-[100%] flex justify-between items-center ">
-          <span className="font-bold text-xl ">${price}</span>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold p-2 rounded-full ">
-            <img src={cartSvg} className="w-6  h-6 " />
-          </button>
-        </div>
+    <div className=" w-[300px] hover:shadow-sm wrapper flex flex-col justify-center items-center max-w-xs rounded overflow-hidden shadow-lg sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+      <img className="w-32 h-42" src={image} alt="Product" />
+      <div className="px-6 py-4">
+        <Link to={`/product/${id}`}>
+          <div className=" hover:underline hover:text-blue-600 font-bold text-xl mb-2 text-center">
+            {title}
+          </div>
+        </Link>
       </div>
-    </Link>
+
+      <div className="container flex justify-around">
+        <span>⭐ Rating:{rating.rate}</span>
+        <span>Count:{rating.count}</span>
+      </div>
+      <div className="px-6 py-4 w-[100%] flex justify-between items-center ">
+        <span className="font-bold text-xl ">${price}</span>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold p-2 rounded-full ">
+          <img src={cartSvg} className="w-6  h-6 " />
+        </button>
+      </div>
+    </div>
   );
 };
 
