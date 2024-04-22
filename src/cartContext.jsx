08766b86b6baf27f4ed87ff10666da18c9756094
Abdusamadzoda Abdulazizi Abdurasul <155437681+tajikwizard@users.noreply.github.com>
2024-04-22 22:55:46 +0500

    Update cartContext.jsx

--- src/cartContext.jsx
+++ src/cartContext.jsx
@@ -1,1 +1,42 @@
+import React, { createContext, useState, useContext } from 'react';
 
+// Creating the CartContext
+const CartContext = createContext();
+
+// Custom hook to use the CartContext
+export const useCart = () => useContext(CartContext);
+
+// CartProvider component to wrap around your application
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  // Function to add an item to the cart
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  // Function to remove an item from the cart
+  const removeFromCart = (index) => {
+    const newCartItems = [...cartItems];
+    newCartItems.splice(index, 1);
+    setCartItems(newCartItems);
+  };
+
+  // Function to clear the cart
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
