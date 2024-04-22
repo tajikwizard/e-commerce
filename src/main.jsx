f04e87d6d033eb851ca02471af4f30baed6ca6d8
Abdusamadzoda Abdulazizi Abdurasul <155437681+tajikwizard@users.noreply.github.com>
2024-04-22 23:23:34 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cart from "./components/Cart/Cart.jsx";
 import Products from "./components/Products/Products.jsx";
-import {CartProvider} from "./cartContext.jsx";
+import { CartProvider } from "./cartContext.jsx";
 import ProductDetails from "./components/Products/ProductDetails.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -22,17 +23,16 @@ const router = createBrowserRouter([
       },
       {
         path: "/product/:productId",
-
         element: <ProductDetails />,
       },
     ],
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router}>
     <CartProvider>
-    <App />
+      <App />
     </CartProvider>
-    
   </RouterProvider>
 );
